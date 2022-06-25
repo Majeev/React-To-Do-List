2d4524947528499a92a03a446e75eb30b60302d4
Jakub Majewski <majewski.j01@gmail.com>
2022-06-25 17:37:16 +0200

    fix: fix infinite loop while sorting items via priority

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import './App.css';
 import ListItem from "./components/ListItem/ListItem";
 import Todos from './components/Todos/Todos'
-import React,{useState,useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
     const [items, setItems] = useState([])
     const [completedTasks, setCompletedTasks] = useState(0)
     let id = Math.random();
 
-    useEffect(() => {
-        const sorted = [...items]
-       setItems(sorted.sort((a, b) => (a.priority > b.priority) ? 1 : -1))
-    },[])
 
-
-    const addItemHandler = (itemName,option) => {
+    const addItemHandler = (itemName, option) => {
         setItems(prevState => ([...prevState, {name: itemName, priority: option, id}]))
     }
     const removeItemHandler = (todo) => {
         return () => {
-      setItems(items.filter(item => item.name !== todo.name))
-        setCompletedTasks(prevState => prevState +1 )}
+            setItems(items.filter(item => item.name !== todo.name))
+            setCompletedTasks(prevState => prevState + 1)
+        }
     }
 
-  return (
-    <div className="App">
-      <ListItem onAddItem={addItemHandler} completedTasks={completedTasks}/>
-        <Todos items={items} onClick={removeItemHandler}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ListItem onAddItem={addItemHandler} completedTasks={completedTasks}/>
+            <Todos items={items} onClick={removeItemHandler}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -5,13 +5,13 @@ import { faTrash } from '@fortawesome/fontawesome-free-solid'
 
 
 const Todos = (props) => {
-
+    const sortedTasks = props.items.sort((a, b) => (a.priority > b.priority) ? 1 : -1)
 
     return (
         <Card className={props.items.length === 0 && css.todoCard}>
 
             <ul>
-                {props.items.map(item => (
+                {sortedTasks.map(item => (
                     <li className={item.priority === '3' ? css.low
                         : item.priority === '2' ? css.moderate
                             : css.high} key={item.id} >{item.name}
